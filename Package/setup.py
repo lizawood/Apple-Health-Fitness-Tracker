673be8de3f6b1d0cf5ec86de9f8ea32ee41ababd
lizawood <liza.4.bc@gmail.com>
2019-07-03 17:29:45 -0700

    Updated setup file

--- Package/setup.py
+++ Package/setup.py
@@ -6,7 +6,6 @@ setup(
     packages=find_packages(exclude=['*test*']),
     license='MIT',
     description='This package provides some basic tools to analyze the health data in a csv file downloaded from Apple Health.',
-    install_requires= 'Numpy;Pandas;Pygal;IPython',
     url='https://github.com/lizawood/Apple-Health-Fitness-Tracker',
     author='Liza Wood',
     author_email='liza.4.bc@gmail.com'
